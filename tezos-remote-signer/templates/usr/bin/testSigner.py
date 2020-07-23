0c6603e8843fb773388cd5cb71e45926bf05f1dc
Nicolas Ochem <nicolas.ochem@gmail.com>
2020-07-24 00:57:03 +0200

    make the signer test do two things (test ledger and test signing daemon) at once

--- tezos-remote-signer/templates/usr/bin/testSigner.py
+++ tezos-remote-signer/templates/usr/bin/testSigner.py
@@ -2,6 +2,7 @@
 import http.server
 import socketserver
 from urllib.parse import urlparse, parse_qs
+import requests
 import subprocess
 import sys
 PORT = 5732
@@ -11,22 +12,25 @@ class LedgerHTTPHandler(http.server.BaseHTTPRequestHandler):
         try:
             query_components = parse_qs(urlparse(self.path).query)
             ledger_url = query_components["ledger_url"]
-            signer_response = subprocess.run(SIGNER_CHECK_ARGS + ledger_url, capture_output=True)
-            if signer_response.returncode == 0:
+            ledger_response = subprocess.run(SIGNER_CHECK_ARGS + ledger_url, capture_output=True)
+            public_baking_key = query_components["public_baking_key"][0]
+            signer_response = requests.get('http://localhost:8443/keys/%s' % public_baking_key)
+            if ledger_response.returncode == 0 and signer_response:
                 self.send_response(200)
             else:
                 self.send_response(500)
             self.send_header("Content-type", "plain")
             self.end_headers()
             try:
-                self.wfile.write(signer_response.stdout)
-                self.wfile.write(signer_response.stderr)
+                self.wfile.write(ledger_response.stdout)
+                self.wfile.write(ledger_response.stderr)
+                self.wfile.write(signer_response.content)
             except ConnectionError:
                 pass
         except Exception as e:
-            self.send_error(301, message=str(e))
+            self.send_error(501, message=str(e))
 if __name__ == '__main__':
     server_address = ('', PORT)
     httpd = http.server.HTTPServer(server_address, LedgerHTTPHandler)
-    print("Ledger status listening at port", PORT)
+    print("Signer status listening at port", PORT)
     httpd.serve_forever()
